5479d8982c454339e0f6910842818ef9d95a812d
GIVEMEFOOD <danielallcorn5@gmail.com>
2026-01-21 23:10:42 +0000

    Update Plugin_Script.js

--- Display/JavaScript/Plugin_Script.js
+++ Display/JavaScript/Plugin_Script.js
@@ -46,76 +46,24 @@ async function loadData() {
             try {
                 const shipsResponse = await fetch(`${baseUrl}/${plugin.name}/ships.json`);
                 if (shipsResponse.ok) {
-                    const rawText = await shipsResponse.text();
-                    if (rawText.includes("version")) {
-                        hardFindPluginFolder = await fetch(`${apiServerUrl}/repos/${repoUrl}/contents/data/${plugin.name}?ref=main`)
-                        extractHardFindPluginFolder = await hardFindPluginFolder.json();
-
-                        const shipsUrl = extractHardFindPluginFolder.find(f => f.name === 'ships.json')?.download_url;
-
-                        if (!shipsUrl) {
-                            console.warn('ships.json not found');
-                        } else {
-                            fetchedShipsData = await fetch(shipsUrl);
-                            pluginData.ships = await fetchedShipsData.json();
-                            loadedSomething = true;
-                        }
-
-                    } else {
                         pluginData.ships = await shipsResponse.json();
                         loadedSomething = true;
-                    }
-
                 } else {
                     console.warn(`${plugin.name}: ships.json not found (${shipsResponse.status})`);
                 }
             
                 const variantsResponse = await fetch(`${baseUrl}/${plugin.name}/variants.json`);
                 if (variantsResponse.ok) {
-                    const rawText = await variantsResponse.text();
-                    if (rawText.includes("version")) {
-                        hardFindPluginFolder = await fetch(`${apiServerUrl}/repos/${repoUrl}/contents/data/${plugin.name}?ref=main`)
-                        extractHardFindPluginFolder = await hardFindPluginFolder.json();
-
-                        const variantsUrl = extractHardFindPluginFolder.find(f => f.name === 'variants.json')?.download_url;
-
-                        if (!variantsUrl) {
-                            console.warn('variants.json not found');
-                        } else {
-                            fetchedVariantsData = await fetch(variantsUrl);
-                            pluginData.variants = await fetchedVariantsData.json();
-                            loadedSomething = true;
-                        }
-
-                    } else {
-                        pluginData.variants = await variantsResponse.json();
-                        loadedSomething = true;
-                    }
+                    pluginData.variants = await variantsResponse.json();
+                    loadedSomething = true;
                 } else {
                     console.warn(`${plugin.name}: variants.json not found (${variantsResponse.status})`);
                 }
             
                 const outfitsResponse = await fetch(`${baseUrl}/${plugin.name}/outfits.json`);
                 if (outfitsResponse.ok) {
-                    const rawText = await outfitsResponse.text();
-                    if (rawText.includes("version")) {
-                        hardFindPluginFolder = await fetch(`${apiServerUrl}/repos/${repoUrl}/contents/data/${plugin.name}?ref=main`)
-                        extractHardFindPluginFolder = await hardFindPluginFolder.json();
-
-                        const outfitsUrl = extractHardFindPluginFolder.find(f => f.name === 'outfits.json')?.download_url;
-
-                        if (!outfitsUrl) {
-                            console.warn('outfits.json not found');
-                        } else {
-                            fetchedOutfitData = await fetch(outfitsUrl);
-                            pluginData.outfits = await fetchedOutfitData.json();
-                            loadedSomething = true;
-                        }
-
-                    } else {
                         pluginData.outfits = await outfitsResponse.json();
                         loadedSomething = true;
-                    }
                 } else {
                     console.warn(`${plugin.name}: outfits.json not found (${outfitsResponse.status})`);
                 }
