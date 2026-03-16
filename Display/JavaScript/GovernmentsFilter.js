0752720f4053e207811b5d00091edd562e00481d
GIVEMEFOOD <ravenslittlesis@gmail.com>
2026-03-16 13:31:01 +0000

    Update GovernmentsFilter.js

--- Display/JavaScript/GovernmentsFilter.js
+++ Display/JavaScript/GovernmentsFilter.js
@@ -123,12 +123,14 @@ function governmentFilterDisplay() {
         if (lastGovernmentData.length) {
             filterTitle.classList.remove("filter-title-no-margin");
             filterTitle.classList.add("filter-title");
+            filterTitle.innerHTML = 'Filter by Government: 🡇'
             populateGovernmentFilters(lastGovernmentData);
         }
     } else {
         filterOptions.innerHTML = ''; // clear to reduce DOM load
         filterTitle.classList.remove("filter-title");
         filterTitle.classList.add("filter-title-no-margin");
+        filterTitle.innerHTML = 'Filter by Government: 🡆'
     }
 }
 
